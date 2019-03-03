5540d062c826ebf491a33862ed21d380ac5149fd
lawscott1988 <lawscott1988@googlemail.com>
2019-03-03 17:29:41 +0000

    finished part 1 and part 2

--- EXERCISE.h
+++ EXERCISE.h
@@ -1,11 +1,41 @@
 #pragma once
 
+#include<string>
+#include<ostream>
+
 //This is for part 1
 class ILogger {
-
+public:
+    //must 'assign' virtual function as = to 0 to make it pure virtual
+    virtual ILogger& LogInfo(const std::string& s) = 0;
+    virtual ILogger& LogError(const std::string& s) = 0;
+    virtual const std::string& GetLastError() const = 0;
+    virtual ~ILogger() = default;
 };
 
 // This is for Part 2/3
-class StringLogger {
+class StringLogger : public ILogger {
+    std::ostream& stream;
+    std::string lastError;
+
+public:
+    StringLogger(std::ostream& os)
+        :   stream(os)
+    {}
+
+    ILogger& LogInfo(const std::string& s) override {
+        stream << "INFO: " << s;
+        return *this;
+    }
+
+    ILogger& LogError(const std::string& s) override {
+        stream << "ERROR: " << s;
+        lastError = s;
+        return *this;
+    }
+
+    const std::string& GetLastError() const override{
+        return lastError;
+    }
 
 };
---
+++ cmake-build-debug/CMakeFiles/OOP_Lesson2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_Lesson2.dir/main.cpp.obj"
+  "OOP_Lesson2.pdb"
+  "OOP_Lesson2.exe"
+  "OOP_Lesson2.exe.manifest"
+  "libOOP_Lesson2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_Lesson2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
